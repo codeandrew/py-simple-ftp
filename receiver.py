29e9a4f5ba152b89cc6cba499c63a15c4432dbfa
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-04-04 19:22:41 +0800

    Add Receive

--- receiver.py
+++ receiver.py
@@ -9,3 +9,12 @@ port = 21
 s.connect((host, port))
 print("Connected ...")
 
+filename = input( str("Please enter a filename for the incoming file : ") )
+file = open(filename, 'wb')
+file_data = s.recv(1024)
+file.write(file_data)
+file.close()
+
+print("File has been received succesfully.")
+
+
